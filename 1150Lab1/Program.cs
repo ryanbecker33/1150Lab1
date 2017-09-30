019976ea698c01791e9a132cdfcc94f15ab6f6ba
ryanbecker33 <rjb82@pitt.edu>
2017-09-30 14:52:56 -0400

    Set up basic socket and project

---
+++ 1150Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _1150Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostname = "api.wunderground.com";
+            int port = 80;
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress host = Dns.GetHostEntry(hostname).AddressList[0];
+            IPEndPoint hostep = new IPEndPoint(host, port);
+            sock.Connect(hostep);
+
+            string request = "GET /api/b0a73c25c2f40b1b/conditions/q/CA/San Francisco.xml\r\n" + "HTTP/1.1 Host: api.wunderground.com\r\n";
+            int response = sock.Send(Encoding.UTF8.GetBytes(request));
+
+            Console.WriteLine(host);
+            Console.ReadLine();
+        }
+    }
+}
